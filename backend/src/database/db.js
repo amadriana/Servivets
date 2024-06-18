931b8cbb200aa5bbd2b4e7f078c22e8e0a56b77e
amadriana <adrianamilenaal@ufps.edu.co>
2024-06-17 21:01:21 -0500

    commit

---
+++ backend/src/database/db.js
@@ -0,0 +1,10 @@
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  define: { timestamps: false },
+});
+
+export default sequelize;
